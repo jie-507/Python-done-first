8af05a32b1f3066ad05cff8ef5fcc24d156b64cf
黄智杰 <3244491838@qq.com>
2025-03-20 18:52:22 +0800

    Add files via upload

---
+++ 3if-elif.py
@@ -0,0 +1,28 @@
+results = 76
+if 100 >= results >= 90:
+    print("A")
+elif 90 > results >= 80:
+    print("B")
+elif 80 > results >= 60:
+    print("C")
+else:
+    print("D")
+
+num = 23
+if num > 0:
+    print("num大于0")
+    if num % 2 == 0:
+        print("num是偶数")
+    else:
+        print("num是奇数")
+else:
+    print("num小于0")
+
+skill = 100
+click = 1
+if skill >= 100:
+    print("大招准备就绪")
+    if click == 1:
+        print("大招已发送")
+    else:
+        print("大招未发送")
--- main.py
+++ main.py
@@ -1,19 +1,44 @@
-# This is a sample Python script.
+ticket = True
+contraband = True
+if ticket:
+    print("车票通过，准备安检")
+    if contraband:
+        print("不可以上车")
+    else:
+        print("安检通过")
+else:
+    print("请先购票")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+age = 13
+if age >= 12:
+    print("负刑事责任")
+else:
+    print("未达到负刑事责任")
+
+a = 1900
+if a % 4 != 0:
+    print(f"{a}不是闰年")
+elif a % 100 == 0 and a % 400 != 0:
+    print(f"{a}不是闰年")
+else:
+    print(f"{a}是闰年")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+number = 123
+a1 = number // 100
+a2 = number // 60
+a3 = number % 60
+result = a3*100 + a2*10+a1
+print(result)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-print ("Hello world")
-print("a")
-print("b")
+studentName = ("小明", "小叶", "小曲")
+name = "路飞"
+mathScore = (90, 80, 85)
+if name in studentName:
+    print("yes")
+else:
+    print("no")
+print(f"{studentName[0]}的数学成绩是{mathScore[0]}")
+print(f"{studentName[1]}的数学成绩是{mathScore[1]}")
+print(f"{studentName[2]}的数学成绩是{mathScore[2]}")
