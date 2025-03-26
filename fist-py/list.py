3dd7f80b3adddbb4df23cd297e71c2820d6864f6
hzj-beginner <3244491838@qq.com>
2025-03-26 18:30:27 +0800

    7.2

--- fist-py/list.py
+++ fist-py/list.py
@@ -11,9 +11,15 @@ print(gasd)
 # 删除pop，括号内填索引即可，不填默认删除最后一个
 gasd.pop(1)
 print(gasd)
-# 查找内容个数，len
+# 访问列表长度，即列表中数据的个数，len(序列名)
 print(len(gasd))
-
+# 序列.index(数据，开始下标，结束下标），还能查找该数据的位置，只填数据就行,不存在就会报错
+# print(gasd.index(150))  ,报错
+print(gasd.index(15))  # 说明15在1的位置
+print(gasd.index(15, 0, 2))  # 存在则出1，若查找不存在则报错
+# 统计出现次数，序列名.count（），不存在就会出现0
+gasd[5] = 15
+print(gasd.count(15))  # 有两个15，所以输出2
 
 # 练习
 rank_list = [100, 98, 88, 80, 75, 64]
@@ -34,3 +40,5 @@ rank_list.insert(1, 99)
 rank_list.insert(2, 98)
 rank_list.insert(4, 87)
 print(rank_list)
+# not in 判断不在，与in判断一样的
+print(60 not in rank_list)
